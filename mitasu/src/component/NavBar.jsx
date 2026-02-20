0b0fbb2f723f1edbdd790c6c0e6e33d8a8dff9b2
Akmal Hanif Rafii <ahhikaru@gmail.com>
2026-02-20 09:16:09 +0900

    fix: Navbar

--- mitasu/src/component/NavBar.jsx
+++ mitasu/src/component/NavBar.jsx
@@ -13,44 +13,44 @@ function NavBar() {
                 <nav className="flex m-5">
                     <div className="grow m-auto">
                         <NavLink to="/">
-                            <span className="mr-15 ml-5">Mitasu</span>
+                            <span className="mr-15 ml-5 hover:bg-neutral-500 cursor-pointer rounded-full p-2">Mitasu</span>
                         </NavLink>
                         <NavLink to="/mybookmark">
-                            <span className="mr-15 ml-15">MyBookmark</span>
+                            <span className="mr-15 ml-15 hover:bg-neutral-500 cursor-pointer rounded-full p-2">MyBookmark</span>
                         </NavLink>
                         <NavLink to="/recommend">
-                            <span className="mr-15 ml-15">Recommend</span>
+                            <span className="mr-15 ml-15 hover:bg-neutral-500 cursor-pointer rounded-full p-2">Recommend</span>
                         </NavLink>
                         <NavLink to="/top-anime">
-                            <span className="mr-15 ml-15">Top Anime</span>
+                            <span className="mr-15 ml-15 hover:bg-neutral-500 cursor-pointer rounded-full p-2">Top Anime</span>
                         </NavLink>
                         <NavLink to="/search-anime">
-                            <span className="mr-15 ml-15">Search</span>
+                            <span className="mr-15 ml-15 hover:bg-neutral-500 cursor-pointer rounded-full p-2">Search</span>
                         </NavLink>
                     </div>
                     <div className="m-auto">
-                        <a className="cursor-pointer" onClick={handleLogout}>Log out</a>
+                        <a className="cursor-pointer hover:bg-neutral-500 cursor-pointer rounded-full p-2" onClick={handleLogout}>Log out</a>
                     </div>
                 </nav>
             ) : (
                 <nav className="flex m-5">
                     <div className="grow m-auto">
                         <NavLink to="/">
-                            <span className="mr-15 ml-5">Mitasu</span>
+                            <span className="mr-15 ml-5 hover:bg-neutral-500 cursor-pointer rounded-full p-2">Mitasu</span>
                         </NavLink>
                         <NavLink to="/top-anime">
-                            <span className="mr-15 ml-15">Top Anime</span>
+                            <span className="mr-15 ml-15 hover:bg-neutral-500 cursor-pointer rounded-full p-2">Top Anime</span>
                         </NavLink>
                         <NavLink to="/search-anime">
-                            <span className="mr-15 ml-15">Search</span>
+                            <span className="mr-15 ml-15 hover:bg-neutral-500 cursor-pointer rounded-full p-2">Search</span>
                         </NavLink>
                     </div>
                     <div className="m-auto">
                         <NavLink to="/login">
-                            <span className="mr-15 ml-15">Log in</span>
+                            <span className="mr-15 ml-15 hover:bg-neutral-500 cursor-pointer rounded-full p-2">Log in</span>
                         </NavLink>
                         <NavLink to="/register">
-                            <span className="mr-15 ml-15">Register</span>
+                            <span className="mr-15 ml-15 hover:bg-neutral-500 cursor-pointer rounded-full p-2">Register</span>
                         </NavLink>
                     </div>
                 </nav>
